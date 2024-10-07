3428e32c0a961501dc326d149bf0149dc3bfa2c3
Abu Mubarak <ismaildeve070@gmail.com>
2024-10-07 23:10:06 +0100

    feat: Add protected route

--- app/navigators/AppStack.tsx
+++ app/navigators/AppStack.tsx
@@ -1,10 +1,30 @@
+import auth from '@react-native-firebase/auth';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Header} from '../components';
+
 import {Onboarding, Verification, VerificationStatus} from '../screens';
 import {AppStackParamList} from './AppStackParamList';
 export const AppStack = () => {
   const Stack = createNativeStackNavigator<AppStackParamList>();
+
+  const [user, setUser] = useState();
+  const [initializing, setInitializing] = useState(true);
+
+  function onAuthStateChanged(users: any) {
+    setUser(users);
+    if (initializing) {
+      setInitializing(false);
+    }
+  }
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // eslint-disable-next-line curly
+  if (initializing) return <></>;
 
   return (
     <Stack.Navigator
@@ -13,20 +33,29 @@ export const AppStack = () => {
         headerShown: false,
         contentStyle: {backgroundColor: 'white'},
       }}>
-      <Stack.Screen name="Onboarding" component={Onboarding} />
-
-      <Stack.Screen
-        name="Verification"
-        component={Verification}
-        options={{
-          headerShown: true,
-          headerShadowVisible: false,
-          headerTitle: '',
-          headerLeft: () => <Header />,
-          headerBackTitleVisible: false,
-        }}
-      />
-      <Stack.Screen name="VerificationStatus" component={VerificationStatus} />
+      {user ? (
+        <>
+          <Stack.Screen
+            name="Verification"
+            component={Verification}
+            options={{
+              headerShown: true,
+              headerShadowVisible: false,
+              headerTitle: '',
+              headerLeft: () => <Header />,
+              headerBackTitleVisible: false,
+            }}
+          />
+          <Stack.Screen
+            name="VerificationStatus"
+            component={VerificationStatus}
+          />
+        </>
+      ) : (
+        <>
+          <Stack.Screen name="Onboarding" component={Onboarding} />
+        </>
+      )}
     </Stack.Navigator>
   );
 };
